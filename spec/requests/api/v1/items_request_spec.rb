4757847a6665fd7d43a5c586b6eb6ad3b2b879c2
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-07 00:05:47 -0700

    tests for create item endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -16,6 +16,9 @@ RSpec.describe 'Items API' do
       expect(item).to have_key(:id)
       expect(item[:id]).to be_a(Integer)
 
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+
       expect(item).to have_key(:description)
       expect(item[:description]).to be_a(String)
 
@@ -36,10 +39,31 @@ RSpec.describe 'Items API' do
     expect(item).to have_key(:id)
     expect(item[:id]).to eq(id)
 
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+
     expect(item).to have_key(:description)
     expect(item[:description]).to be_a(String)
 
     expect(item).to have_key(:unit_price)
     expect(item[:unit_price]).to be_a(Float)
+  end
+
+  it 'can create a new item' do
+    item_params = {
+      name: 'Washing Machine',
+      description: 'Washes clothes',
+      unit_price: 500.0
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
   end
 end
