701315ba11aeb0f9ce457319ada548eaab08849d
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-07 00:28:46 -0700

    delete item tests

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -81,4 +81,15 @@ RSpec.describe 'Items API' do
     expect(item.name).to_not eq(previous_name)
     expect(item.name).to eq('Dishwasher')
   end
+
+  it 'can delete an existing item' do
+    item = create(:item)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+  end
 end
