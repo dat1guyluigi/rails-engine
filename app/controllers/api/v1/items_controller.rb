199d5edf35e2a0db7601379a9497c66de4ef6f0d
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-12-08 19:01:36 -0700

    refactor controller for use with serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,27 +1,38 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :item, only: [:show, :update, :destroy]
+
   def index
-    render json: Item.all
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    render json: Item.find(params[:id])
+    render json: ItemSerializer.new(@item)
   end
 
   def create
-    render json: Item.create(item_params)
+    item = Item.create(item_params)
+    if item.save
+      render json: ItemSerializer.new(item)
+    end
   end
 
   def update
-    render json: Item.update(params[:id], item_params)
+    if @item.update(item_params)
+      ItemSerializer.new(@item)
+    end
   end
 
   def destroy
-    render json: Item.delete(params[:id])
+    ItemSerializer.new(@item.destroy)
   end
 
   private
 
+  def item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price)
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
   end
 end
